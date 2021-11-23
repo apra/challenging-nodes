e505deb893a73f6d93d16204a8f2356bf7c7a2a6
Samuele Papa <samuele.papa@gmail.com>
2021-11-23 10:06:04 +0100

    test CUDA LISA

--- crfill/docker_folder/test_cuda_pytorch.py
+++ crfill/docker_folder/test_cuda_pytorch.py
@@ -4,5 +4,5 @@ available_gpus = [torch.cuda.device(i) for i in range(torch.cuda.device_count())
 for i in available_gpus:
     print(i)
 f = open("out.txt", "a")
-f.write(torch.cuda.is_available())
+f.write(str(torch.cuda.is_available()))
 f.close()
