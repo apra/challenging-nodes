dc4fb7ad0b9e34e8133b522e835476dc4c88c5bf
Samuele Papa <samuele.papa@gmail.com>
2021-11-24 21:56:00 +0100

    Start interactive data folder

--- crfill/cluster/start_interactive.sh
+++ crfill/cluster/start_interactive.sh
@@ -8,6 +8,6 @@ DATA="$TMPDIR/spapa"
 
 LOGGING_DIR="$HOME/challenging-nodes/crfill/checkpoints"
 
-STANDARD_PARAMS="--dataset_mode_train custom_train --dataset_mode custom_train --train_image_dir /data/images --train_nodule_list /data/metadata.csv --netG twostagend --netD deepfill --preprocess_mode none --validation_freq 2000 --niter 100 --display_freq 500 --model arrange"
+STANDARD_PARAMS="--dataset_mode_train custom_train --dataset_mode custom_train --train_image_dir /data/data/images --train_nodule_list /data/data/metadata.csv --netG twostagend --netD deepfill --preprocess_mode none --validation_freq 2000 --niter 100 --display_freq 500 --model arrange"
 
 python -u train.py --name $NAME --num_workers 16 --checkpoints_dir $LOGGING_DIR/$NAME --gpu_ids 0,1 --batchSize 48 $STANDARD_PARAMS
