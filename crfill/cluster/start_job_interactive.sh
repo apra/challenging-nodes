03bb7e77a1f23bd77095b3092d148d9727b97a26
Samuele Papa <samuele.papa@gmail.com>
2021-11-24 20:44:51 +0100

    Runing singularity with shell to install packages manually

--- crfill/cluster/start_job_interactive.sh
+++ crfill/cluster/start_job_interactive.sh
@@ -10,26 +10,33 @@ cp -r $HOME/data/ "$DATA"
 
 LOGGING_DIR="$HOME/challenging-nodes/crfill/checkpoints"
 
-STANDARD_PARAMS="--dataset_mode_train custom_train --dataset_mode custom_train --train_image_dir /data/images --train_nodule_list /data/metadata.csv --netG twostagend --netD deepfill --preprocess_mode none --validation_freq 2000 --niter 100 --display_freq 500 --model arrange"
-
-run_train()
-{
-  echo "python -u train.py --name $NAME --num_workers 16 --checkpoints_dir $LOGGING_DIR/$NAME --gpu_ids 0,1 --batchSize 48 $STANDARD_PARAMS"
-}
-COMMAND=$(run_train)
-
-slurm_submit()
-{
-singularity exec --no-home --nv \
+singularity shell --no-home --nv \
     --bind "$DATA":/data \
     --bind "$LOGGING_DIR" \
     --bind "$JOBS_SOURCE" \
     --pwd "$JOBS_SOURCE" \
-    $SINGULARITYIMAGE \
-    $COMMAND
-}
-
-echo $(slurm_submit $COMMAND)
+    $SINGULARITYIMAGE
+#
+#STANDARD_PARAMS="--dataset_mode_train custom_train --dataset_mode custom_train --train_image_dir /data/images --train_nodule_list /data/metadata.csv --netG twostagend --netD deepfill --preprocess_mode none --validation_freq 2000 --niter 100 --display_freq 500 --model arrange"
+#
+#run_train()
+#{
+#  echo "python -u train.py --name $NAME --num_workers 16 --checkpoints_dir $LOGGING_DIR/$NAME --gpu_ids 0,1 --batchSize 48 $STANDARD_PARAMS"
+#}
+#COMMAND=$(run_train)
+#
+#slurm_submit()
+#{
+#singularity exec --no-home --nv \
+#    --bind "$DATA":/data \
+#    --bind "$LOGGING_DIR" \
+#    --bind "$JOBS_SOURCE" \
+#    --pwd "$JOBS_SOURCE" \
+#    $SINGULARITYIMAGE \
+#    $COMMAND
+#}
+#
+#echo $(slurm_submit $COMMAND)
 
 #singularity shell --no-home --nv \
 #    --bind "$DATA":/data \
