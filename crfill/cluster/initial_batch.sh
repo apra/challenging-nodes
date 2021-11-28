f975ce0471a121fe3438b122ba19bfedb06aa172
Samuele Papa <samuele.papa@gmail.com>
2021-11-28 21:19:00 +0100

    Remove resampling

--- crfill/cluster/initial_batch.sh
+++ crfill/cluster/initial_batch.sh
@@ -24,7 +24,7 @@ cp -ra $HOME/data/data_node21/. "$DATA"
 
 NAME=full_datasets_2gpus_beta_l15_lambda_feat05
 
-STANDARD_PARAMS="--seed 0  --batchSize 40 --include_chexpert --include_mimic --node21_resample_count 10 --dataset_mode_train custom_train --dataset_mode custom_train --train_image_dir /data --netG twostagend --netD deepfill --preprocess_mode none --validation_freq 20000 --niter 600 --display_freq 2000 --model arrange"
+STANDARD_PARAMS="--seed 0  --batchSize 40 --include_chexpert --include_mimic --node21_resample_count 0 --dataset_mode_train custom_train --dataset_mode custom_train --train_image_dir /data --netG twostagend --netD deepfill --preprocess_mode none --validation_freq 20000 --niter 600 --display_freq 2000 --model arrange"
 
 COMMAND="python -u train.py --name $NAME --num_workers $NUM_WORKERS --checkpoints_dir $LOGGING_DIR/$NAME --gpu_ids 0,1 --beta_l1 1.5 --lambda_feat 0.5 $STANDARD_PARAMS"
 
