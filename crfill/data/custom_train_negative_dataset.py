Fixed rcnn gradients, fixed random bbox implementation

crfill/data/custom_train_negative_dataset.py
@@ -40,7 +40,7 @@ class CustomTrainNegativeDataset(BaseDataset):
         size = len(self.paths)
         self.full_dataset_size = size
         # TODO: check whether below the right amount of bboxes are created -- in combination with the k-fold
-        self.bboxes = create_random_bboxes(number_of_bboxes=self.full_dataset_size)  # TODO make reproducible RNG here
+        self.bboxes = create_random_bboxes(number_of_bboxes=self.full_dataset_size, max_x=opt.load_size, max_y=opt.load_size)  # TODO make reproducible RNG here
         self.fold_size = int(self.full_dataset_size / opt.num_folds)
         self.begin_fold_idx = opt.fold * self.fold_size
         if opt.fold == opt.num_folds - 1:
@@ -100,12 +100,9 @@ class CustomTrainNegativeDataset(BaseDataset):
             cropped_image = np.array(normalize_cxr(cropped_image), dtype='float32')  # divide 4095
             _, mask_array = mask_image(cropped_image, new_mask_bbox)
 
-            # params = get_params(self.opt, cropped_image.shape)
             crop_bbox = [image_mask_bbox[0] - new_mask_bbox[0], image_mask_bbox[1] - new_mask_bbox[1], crop_size, crop_size]
-            #_, full_image_mask = mask_image(full_image, crop_bbox)
 
             mask_tensor = torch.Tensor(mask_array)
-            #full_image_mask_tensor = torch.Tensor(full_image_mask)
             full_image_crop_bbox = torch.Tensor(crop_bbox)
             full_image_bbox = torch.Tensor([image_mask_bbox[0], image_mask_bbox[1], image_mask_bbox[0]+image_mask_bbox[2], image_mask_bbox[1]+image_mask_bbox[3]])
             full_image_tensor = torch.unsqueeze(torch.Tensor(full_image), 0)  # don't self.transform this -- rcnn does its own normalization

crfill/data/custom_transformations.py
@@ -24,20 +24,20 @@ def mask_convention_setter(mask, invert=False):
         return mask
 
 
-def create_random_bboxes(number_of_bboxes, seed=0):
+def create_random_bboxes(number_of_bboxes, seed=0, max_x=1024, max_y=1024):
     # TODO: make reproducible rng
     bbox_list = []
     for i in range(number_of_bboxes):
         # distributions that match closely what is found in the data
         l_or_r = np.random.rand()  # x has this left and right factor because it occurs in lungs, not in between lungs
-        if l_or_r > 0.5:
-            x = min(930, np.random.normal(725, 80))
-        else:
-            x = max(20, np.random.normal(225, 80))
+        if l_or_r > 0.5:  # right lung
+            x = min(930, max(np.random.normal(725, 80), 530))
+        else:  # left lung
+            x = max(20, min(np.random.normal(225, 80), 450))
 
-        y = (np.random.beta(2, 2) + (1 / 7)) * 700
-        w = np.random.gamma(8, 7.5)
-        h = np.random.gamma(7, 8.4)
+        y = (np.random.beta(2, 2) + (1 / 7)) * 700  # is bounded [100, 800]
+        w = min(np.random.gamma(8, 7.5), max_x - x, 230)  # 230 is the max of the simulated_metadata, which we will have to predict on
+        h = min(np.random.gamma(7, 8.4), max_y - y, 230)
         bbox_list.append([int(x), int(y), int(w), int(h)])
     return bbox_list
 
@@ -54,17 +54,19 @@ def crop_around_mask_bbox(image: np.ndarray, mask_bbox, crop_size=256, rng=None,
     if rng is None:
         rng = np.random.default_rng(seed=0)
 
-    crop_min_x = max(mask_x + mask_w - crop_size + 1, 0)
-    crop_max_x = min(mask_x , im_max_x - crop_size - 1)
-    crop_min_y = max(mask_y + mask_h - crop_size + 1, 0)
-    crop_max_y = min(mask_y, im_max_y - crop_size - 1)
+    crop_min_x = max(mask_x + mask_w - crop_size, 0)
+    crop_max_x = min(mask_x, im_max_x - crop_size)
+    crop_min_y = max(mask_y + mask_h - crop_size, 0)
+    crop_max_y = min(mask_y, im_max_y - crop_size)
+
+    assert crop_min_x < crop_max_x, f"bbox inputted {mask_bbox}, with min {crop_min_x} and max {crop_max_x}"
 
     if crop_min_y == crop_max_y:
         crop_y = crop_min_y
     else:
         crop_y = rng.integers(crop_min_y, crop_max_y)
 
-    if crop_max_x == crop_max_x:
+    if crop_min_x == crop_max_x:
         crop_x = crop_min_x
     else:
         crop_x = rng.integers(crop_min_x, crop_max_x)
@@ -72,6 +74,10 @@ def crop_around_mask_bbox(image: np.ndarray, mask_bbox, crop_size=256, rng=None,
     cropped_image = crop_to_bbox(image, [crop_y, crop_x, crop_size, crop_size])
     new_mask = [mask_x - crop_x, mask_y - crop_y, mask_w, mask_h]
     new_mask = mask_convention_setter(new_mask)
+
+    assert crop_x + crop_size < im_max_x, f"Crop_x is {crop_x}, such that we find max x of {crop_x + crop_size}, bbox: {mask_bbox}"
+    assert crop_y + crop_size < im_max_y, f"Crop_y is {crop_y}, such that we find max x of {crop_y + crop_size}"
+
     if return_new_mask_bbox:
         return cropped_image, new_mask
     else:

crfill/models/arrangedoubledisc_model.py
@@ -16,6 +16,7 @@ class ArrangedoublediscModel(InpaintdoublediscModel):
     def __init__(self, opt):
         super().__init__(opt)
         _, self.netD_aux, _ = self.initialize_networks(opt)
+
         if opt.continue_train:
             self.netD_aux = util.load_network(self.netD_aux, 'D_aux', opt.which_epoch, opt)
         if opt.load_base_g is not None:
@@ -39,6 +40,7 @@ class ArrangedoublediscModel(InpaintdoublediscModel):
         if opt.isTrain:
             D_params = list(self.netD.parameters()) + \
                     list(self.netD_aux.parameters())
+            DRCNN_params = self.netDRCNN.parameters()
 
         beta1, beta2 = opt.beta1, opt.beta2
         if opt.no_TTUR:
@@ -48,8 +50,9 @@ class ArrangedoublediscModel(InpaintdoublediscModel):
 
         optimizer_G = torch.optim.Adam(G_params, lr=G_lr, betas=(beta1, beta2))
         optimizer_D = torch.optim.Adam(D_params, lr=D_lr, betas=(beta1, beta2))
+        optimizer_DRCNN = torch.optim.Adam(DRCNN_params, lr=0)
 
-        return optimizer_G, optimizer_D
+        return optimizer_G, optimizer_D, optimizer_DRCNN
 
     def forward(self, data, mode):
         inputs, real_image, mask, full_image, full_image_crop_bbox, full_image_bbox = self.preprocess_input(data)
@@ -75,6 +78,7 @@ class ArrangedoublediscModel(InpaintdoublediscModel):
             return composed_image, inputs
         else:
             raise ValueError("|mode| is invalid")
+
 
     def generate_fake(self, inputs, real_image, mask):
         coarse_image, fake_image, aux_image, recon_aux = self.netG(inputs, mask)
@@ -129,7 +133,13 @@ class ArrangedoublediscModel(InpaintdoublediscModel):
         composed_full_image = full_image
         for i in range(len(full_image_crop_bbox)):
             c_x1, c_y1, c_w, c_h = full_image_crop_bbox[i].int()
-            composed_full_image[i, :, c_y1:c_y1+c_h, c_x1:c_x1+c_w] = composed_image[i]
+            try:
+                composed_full_image[i, :, c_y1:c_y1+c_h, c_x1:c_x1+c_w] = composed_image[i]
+            except RuntimeError:
+                print(composed_image[i])
+                print(composed_full_image[i])
+                print(composed_image[i].shape)
+                print(composed_full_image[i].shape)
 
         # large_test = composed_full_image[0].cpu().detach().numpy().reshape((1024,1024))
         # large_test = (large_test * 0.5 + 0.5) * 255

crfill/models/inpaintdoubledisc_model.py
@@ -157,7 +157,7 @@ class InpaintdoublediscModel(torch.nn.Module):
 
         # Faster RCNN loss -- For aux loss, composed_full_image and full_image_bbox will be passed as None
         if composed_full_image is not None:
-            rcnn_losses = self.discriminate_RCNN(composed_full_image, full_image_bbox)
+            rcnn_losses = self.RCNN_loss(composed_full_image, full_image_bbox)
             G_losses['RCNN'] = rcnn_losses
             #G_losses['RCNN'] = self.criterionGAN(pred_rcnn, True, for_discriminator=False)
 
@@ -249,13 +249,18 @@ class InpaintdoublediscModel(torch.nn.Module):
 
         return pred_fake, pred_real
 
-    def discriminate_RCNN(self, composed_image, ground_truth_bbox):
+    def RCNN_loss(self, composed_image, ground_truth_bbox):
         targets = [{'boxes': torch.unsqueeze(bbox, 0),
                     'labels': torch.LongTensor([1]).cuda()} for bbox in ground_truth_bbox]  # label=1 for tumor class
-        discriminator_out = self.netDRCNN(composed_image, targets)
-        relevant_loss = discriminator_out['loss_objectness']
-        # TODO: think about what loss is the correct one to return
-        return relevant_loss
+        loss_dict = self.netDRCNN(composed_image, targets)
+
+        _loss_sum = 0
+        count = 0
+        for key in loss_dict:
+            count += 1
+            _loss_sum += loss_dict[key]
+        # TODO: think about what (sum of) loss is the correct one to return
+        return _loss_sum
 
     # Take the prediction of fake and real images from the combined batch
     def divide_pred(self, pred):

crfill/train_doubledisc.py
@@ -12,7 +12,7 @@ from trainers import create_trainer
 from torch.utils import tensorboard
 #from torch.utils.tensorboard import SummaryWriter
 from util.util import set_all_seeds
-from trainers.pix2pix_trainer import Pix2PixTrainer
+from trainers.pix2pixdoubledisc_trainer import Pix2PixDoubleDiscTrainer
 
 from torchvision import datasets, transforms
 from util.plot_util import draw_bounding_boxes
@@ -31,7 +31,7 @@ dataloader_train, dataloader_val = data.create_dataloader_trainval(opt)
 
 # create trainer for our model
 # trainer = create_trainer(opt)
-trainer = Pix2PixTrainer(opt)
+trainer = Pix2PixDoubleDiscTrainer(opt)
 model = trainer.model
 
 # create tool for counting iterations