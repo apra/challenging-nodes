cb0568f26081f4e900ead99d26672e61acffe397
Samuele Papa <samuele.papa@gmail.com>
2021-12-05 00:32:55 +0100

    name of checkpoint was wrong, fixed now

--- crfill/cluster/parameter_search.sh
+++ crfill/cluster/parameter_search.sh
@@ -32,7 +32,7 @@ LOGGING_DIR="$HOME/challenging-nodes/crfill/checkpoints"
 mkdir -p "$DATA"
 cp -ra $HOME/data/. "$DATA"
 
-NAME="skipconn_2gpus_beta_l$BETAL1_1-lambda_feat$LAMBDA_1"
+NAME="skipconndoubledisc_2gpus_beta_l$BETAL1_1-lambda_feat$LAMBDA_1"
 
 STANDARD_PARAMS="--seed 0 --batchSize 40 --niter_decay 8 --include_chexpert --include_mimic --node21_resample_count 10 --dataset_mode_train custom_train_all --dataset_mode custom_train --train_image_dir /data --netG twostagend --netD deepfill --preprocess_mode none --validation_freq 20000 --niter 43 --display_freq 5000 --model arrangeskipconn"
 
@@ -48,7 +48,7 @@ singularity exec --no-home --nv \
 $SINGULARITYIMAGE \
 $COMMAND &
 echo "$NAME command ran"
-NAME="skipconn_2gpus_beta_l$BETAL1_2-lambda_feat$LAMBDA_2"
+NAME="skipconndoubledisc_2gpus_beta_l$BETAL1_2-lambda_feat$LAMBDA_2"
 
 COMMAND="python -u train.py --name $NAME --num_workers $NUM_WORKERS --checkpoints_dir $LOGGING_DIR/$NAME --gpu_ids 2,3 --beta_l1 $BETAL1_2 --lambda_ref $LAMBDA_2 $STANDARD_PARAMS"
 
