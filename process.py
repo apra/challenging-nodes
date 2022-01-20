f95dd66d20bf8301938824f0e2cff358fa61fb05
Samuele Papa <samuele.papa@gmail.com>
2022-01-20 13:04:52 +0100

    Selection of lesion implemented

--- process.py
+++ process.py
@@ -6,6 +6,7 @@ from evalutils.validators import (
     UniquePathIndicesValidator,
     UniqueImagesValidator,
 )
+import matplotlib
 from utils import *
 from typing import Dict
 import json
@@ -131,7 +132,7 @@ class Nodulegeneration(SegmentationAlgorithm):
             array = np.asarray(array)
             idx = (np.abs(array - value)).argmin()
             return idx
-
+        print(lesion_bbox)
         xcoords = np.array(self.metadata["dim0"])
         ycoords = np.array(self.metadata["dim1"])
 
@@ -140,6 +141,8 @@ class Nodulegeneration(SegmentationAlgorithm):
         areas = xcoords * ycoords
 
         ratio = (lesion_bbox[3] / lesion_bbox[2])
+        area = (lesion_bbox[3] * lesion_bbox[2])
+        scaled_area = area/40
 
         idx = find_nearest(aspect_ratios, ratio)
         inverse_idx = find_nearest(inverse_ratios, ratio)
@@ -157,68 +160,48 @@ class Nodulegeneration(SegmentationAlgorithm):
         with torch.no_grad():
             candidate_lesions = self.vae.sample(x=loaded_lesion, samples=10, explore_dim=13).cpu()
             bbox, crop_lesion = self.find_lesion(candidate_lesions)
+            heights = bbox[:,3]-bbox[:,1]
+            widths = bbox[:,2]-bbox[:,0]
+            areas = heights * widths
+            area_idx = find_nearest(areas, area)
+            selected_lesion = candidate_lesions[area_idx]
+            selected_lesion_crop = crop_lesion[area_idx]
+            selected_bbox = bbox[area_idx]
+            # plt.figure()
+            # plt.imshow(selected_lesion.permute(1,2,0))
+            # plt.show()
+            # plt.figure()
+            # plt.title("Original one")
+            # plt.imshow(loaded_lesion[0].permute(1, 2, 0).cpu())
+            # plt.show()
             new_a_ratios = (bbox[:,3]-bbox[:,1])/(bbox[:,2]-bbox[:,0])
 
-        # lesion = self.netG.sample(samples=starting_cxr.shape[0], change_dim=13, change_val=.98)
+        min_row = lesion_bbox[1]
+        min_col = lesion_bbox[0]
+        rows = lesion_bbox[3]
+        cols = lesion_bbox[2]
+        max_row = min_row + rows
+        max_col = min_col + cols
+        upscaled_lesion = self.upscale_lesion(
+            selected_lesion_crop, selected_bbox
+        )
+        # input_0 = scaled_down_lesions[sample]
+        input_1 = (
+            starting_cxr
+        )
+        result = (1 / 255) * poisson_blend(
+            upscaled_lesion, input_1, min_col, max_col, min_row, max_row
+        )
+        fig, ax = plt.subplots(1,2, figsize=(10,5), dpi=100)
+        ax = ax.flatten()
+        ax[0].imshow(starting_cxr)
 
-        starting_cxr = (starting_cxr + 1) / 2
-        addition = convert_to_range_0_1(lesion)
-
-        new = addition * 255
-        new = new.squeeze().detach().cpu().numpy().astype("uint8")
-        mask_lesions = torch.zeros_like(addition)
-        generated_bbox = []
-        scaled_down_lesions = []
-
-        for sample in range(mask_lesions.shape[0]):
-            result = cv2.GaussianBlur(new[sample], (11, 11), 5)
-            result = cv2.threshold(
-                result, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU
-            )[1]
-            result = 255 - result
-            non_zero = np.argwhere(result)
-            top_left = non_zero.min(axis=0)
-            bottom_right = non_zero.max(axis=0)
-            generated_bbox.append(
-                (top_left[0], top_left[1], bottom_right[0], bottom_right[1])
-            )
-            result = (
-                addition[sample][
-                    :,
-                    top_left[0] : bottom_right[0] + 1,
-                    top_left[1] : bottom_right[1] + 1,
-                ]
-                .detach()
-                .cpu()
-                .numpy()
-            )
-            scaled_down_lesions.append(result.reshape(result.shape[-2:]))
-
-        results = np.zeros_like(starting_cxr.detach().cpu().numpy())
-        for sample in range(results.shape[0]):
-            min_row = lesion_bbox[sample][1].item()
-            min_col = lesion_bbox[sample][0].item()
-            rows = lesion_bbox[sample][3].item()
-            cols = lesion_bbox[sample][2].item()
-            max_row = min_row + rows
-            max_col = min_col + cols
-            upscaled_lesion = self.upscale_lesion(
-                scaled_down_lesions[sample], lesion_bbox[sample]
-            )
-            # input_0 = scaled_down_lesions[sample]
-            input_1 = (
-                starting_cxr[sample]
-                .detach()
-                .cpu()
-                .numpy()
-                .reshape((starting_cxr[sample].shape[-2:]))
-            )
-            results[sample] = (1 / 255) * poisson_blend(
-                upscaled_lesion, input_1, min_col, max_col, min_row, max_row
-            )
-            # results[sample] = torch.Tensor(poisson_edit(addition[sample].permute(1, 2, 0).detach().cpu().numpy(),
-            #                                starting_cxr[sample].permute(1, 2, 0).detach().cpu().numpy(),
-            #                                mask_lesion[sample].permute(1, 2, 0).detach().cpu().numpy(), (0, 0))).permute(2,0,1)
+        ax[1].imshow(result)
+        ax[1].add_patch(matplotlib.patches.Rectangle((lesion_bbox[0], lesion_bbox[1]), lesion_bbox[2], lesion_bbox[3], facecolor="none", ec='k', lw=1))
+        plt.show()
+        # results[sample] = torch.Tensor(poisson_edit(addition[sample].permute(1, 2, 0).detach().cpu().numpy(),
+        #                                starting_cxr[sample].permute(1, 2, 0).detach().cpu().numpy(),
+        #                                mask_lesion[sample].permute(1, 2, 0).detach().cpu().numpy(), (0, 0))).permute(2,0,1)
 
         # f, axarr = plt.subplots(1,4)
 
@@ -226,7 +209,7 @@ class Nodulegeneration(SegmentationAlgorithm):
         # draw_rectangle(lesion_bbox.cpu().numpy()[0])
         # plt.show()
 
-        return 2 * (results - 0.5)
+        return result
 
     def predict(self, *, input_image: SimpleITK.Image) -> SimpleITK.Image:
         input_image = SimpleITK.GetArrayFromImage(input_image)
